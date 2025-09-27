eb7e2d1e1a7f83e5ec4cd8e7daf4b726bdc240a7
vaibhav-belekar <vaibhavsbelekar@gmail.com>
2025-09-28 02:49:02 +0530

    add dictionary

---
+++ python basic/04_dictionary&sets/dictionary.py
@@ -0,0 +1,12 @@
+#learning dictionary
+
+info={
+    "name":"vaibhav",
+    "age":29,
+    "address":"lakhani",
+    "grade":"A"
+}
+
+print(info)
+info["name"]="vaibhav Belekar"
+print("after assign new value",info)
