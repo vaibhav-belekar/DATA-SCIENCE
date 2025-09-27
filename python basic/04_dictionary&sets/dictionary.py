1b7b02a56f5525df860a972109999edf46b5e9ed
vaibhav-belekar <vaibhavsbelekar@gmail.com>
2025-09-28 02:54:47 +0530

    add dictionary

--- python basic/04_dictionary&sets/dictionary.py
+++ python basic/04_dictionary&sets/dictionary.py
@@ -10,3 +10,19 @@ info={
 print(info)
 info["name"]="vaibhav Belekar"
 print("after assign new value",info)
+
+# nested dictonary
+student={
+    "name":"vaibhav belekar",
+    "subjects":{
+        "chem":97,
+        "math":85,
+        "english":98
+    }
+}
+
+print(student)
+print(student["subjects"]["chem"])
+
+
+#method
